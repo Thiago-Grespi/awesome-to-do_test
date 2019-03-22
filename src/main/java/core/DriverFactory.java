b654360b47568d45768b18e77d2fa7980b0fd491
Thiago-Grespi <thiago.grespi90@gmail.com>
2019-03-22 00:48:02 -0300

    possibilitando a escolha do browser

--- src/main/java/core/DriverFactory.java
+++ src/main/java/core/DriverFactory.java
@@ -1,6 +1,9 @@
 package core;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import static core.Propriedades.Browsers.CHROME;
 
 public class DriverFactory {
 
@@ -10,7 +13,12 @@ public class DriverFactory {
 
     public static WebDriver getDriver(){
         if(driver == null){
-            driver = new ChromeDriver();
+
+            switch (Propriedades.browsers){
+                case CHROME: driver = new ChromeDriver(); break;
+                case FIREFOX: driver = new FirefoxDriver(); break;
+            }
+
             driver.manage().window().maximize();
         }
         return driver;
--- src/main/java/core/Propriedades.java
+++ src/main/java/core/Propriedades.java
@@ -3,4 +3,10 @@ package core;
 public class Propriedades {
 
     public static boolean FECHAR_BOWSER = false;
+    public static Browsers browsers = Browsers.CHROME;
+
+    public enum Browsers {
+        CHROME,
+        FIREFOX
+    }
 }
